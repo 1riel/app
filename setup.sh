112fb2a5ed45d002f2e9d226b6c7eddc02bd197f
muysengly-itc <muy.sengly@itc.edu.kh>
2026-03-31 15:21:12 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,35 +1,3 @@
-# ssh to server
-ssh root@msl-t470
-
-
-apt update && apt upgrade -y
-apt install python3-venv -y
-apt install python3-pip -y
-apt install git -y
-apt install nano -y
-apt install docker.io -y
-
-
-
-# clone project from github to a specific directory
-git clone https://github.com/1riel/server.git /root/1riel_server
-
-
-# cd to project directory
-cd /root/1riel_server
-
-
-# write .env file
-touch .env
-
-
-nano .env
-# paste
-
-# linux
-python3 -m venv /.venv
-source /.venv/bin/activate
-
 # windows
 python -m venv .venv
 .venv\Scripts\activate
