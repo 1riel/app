9a2f245e031f248a55d56e633c59e5a8a2cae742
muysengly-itc <muy.sengly@itc.edu.kh>
2026-04-02 12:13:47 +0700

    update

--- client/lib/Telegram_Auth.dart
+++ client/lib/Telegram_Auth.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import 'package:flutter/widgets.dart';
-
 const String telegramBotUsername = 'muy_riel_otp_bot';
 const String telegramAuthUrl = 'https://www.1riel.com/auth/telegram';
 const String telegramWidgetScriptUrl = 'https://telegram.org/js/telegram-widget.js?23';
@@ -26,24 +24,33 @@ class _TelegramLoginScreenState extends State<TelegramLoginScreen> {
     super.initState();
     viewType = '';
 
-    ui_web.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
-      final host = html.DivElement()
-        ..style.width = '100%'
-        ..style.height = '100%'
-        ..style.display = 'flex'
-        ..style.alignItems = 'center'
-        ..style.justifyContent = 'center';
+    ui_web.platformViewRegistry.registerViewFactory(viewType, (int _) {
+      final script = html.ScriptElement();
 
-      final script = html.ScriptElement()
-        ..async = true
-        ..src = telegramWidgetScriptUrl;
-
+      script.async = true;
+      script.src = telegramWidgetScriptUrl;
       script.setAttribute('data-telegram-login', telegramBotUsername);
       script.setAttribute('data-size', 'large');
       script.setAttribute('data-radius', '0');
       script.setAttribute('data-auth-url', telegramAuthUrl);
       script.setAttribute('data-request-access', 'write');
 
+      // <script
+      //       async
+      //       src="https://telegram.org/js/telegram-widget.js?23"
+      //       data-telegram-login="muy_riel_otp_bot"
+      //       data-size="large"
+      //       data-radius="0"
+      //       data-auth-url="https://www.1riel.com/auth/telegram"
+      //       data-request-access="write"
+      //     ></script>
+
+      final host = html.DivElement();
+      // ..style.width = '100%'
+      // ..style.height = '100%'
+      // ..style.display = 'flex'
+      // ..style.alignItems = 'center'
+      // ..style.justifyContent = 'center';
       host.append(script);
       return host;
     });
@@ -52,10 +59,9 @@ class _TelegramLoginScreenState extends State<TelegramLoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Telegram Login 1201')),
+      appBar: AppBar(title: const Text('Telegram Login 1209')),
       body: Center(
-        //
-        child: HtmlElementView(viewType: viewType),
+        child: HtmlElementView(viewType: viewType), //
       ),
     );
   }
